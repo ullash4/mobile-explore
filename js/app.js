6ef0e0eacb8d5b2fafd321736ec73f10f8b75d7a
ullash4 <programmerullash@gmail.com>
2022-03-01 14:51:36 +0600

    final short

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
+// Function for Searched result spinner 
 const loadSpinner = dStyle =>{
     document.getElementById('spinner').style.display=dStyle;
-}
-
+};
+// Get value from search bar 
 const getValue=()=>{
     const inputValue = document.getElementById('search-input');
     const value = inputValue.value;
@@ -11,13 +12,13 @@ const getValue=()=>{
     .then(res => res.json())
     .then(data => showPhone(data.data))
     inputValue.value=''; 
-}
+};
+// Show Phones on UI 
 const showPhone = phones =>{
-    // console.log(phones);
     const div = document.getElementById('injected-div');
     div.textContent='';
     if(phones==false){
-        document.getElementById('fail-sms').style.display='block'
+        document.getElementById('fail-sms').style.display='block';
         loadSpinner('none');
     }else{
         phones.slice(0, 20).forEach(phone => {
@@ -28,7 +29,7 @@ const showPhone = phones =>{
             <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">${phone.phone_name}</h5>
-              <p class="card-text">${phone.brand}</p>
+              <p class="card-text">Brand: ${phone.brand}</p>
               <button type="button" onclick="getDetail('${phone.slug}')" class="btn btn-outline-dark">More Details</button>
             </div>
           </div>`;
@@ -38,22 +39,22 @@ const showPhone = phones =>{
         loadSpinner('none');
     };
 };
-
+// Spinner for Details section 
 const spinnerInDetail = styleD =>{
     document.getElementById('spinner2').style.display=styleD;
-}
-
+};
+// Get details from phone id/slug 
 const getDetail = details =>{
     spinnerInDetail('block');
     const url2 = `https://openapi.programming-hero.com/api/phone/${details}`;
     fetch(url2)
     .then(res => res.json())
     .then(data => showDetail(data.data))
-}
+};
+// Show phone details on UI 
 const showDetail = detailShow =>{
     const div2 = document.getElementById('injected-div2');
     div2.textContent='';
-    
     const newDiv2 = document.createElement('div');
     newDiv2.innerHTML = `
         <div class="card border-0 p-1 shadow-lg p-3 mb-5 bg-body rounded">
